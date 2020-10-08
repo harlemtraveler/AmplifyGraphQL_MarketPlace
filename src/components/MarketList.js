7b84097253f55762d7febe5242c34d680352e873
Robot <tjk315@gmail.com>
2020-10-08 00:42:50 -0400

    Connect Component to Execute Queries - Add MarketList.js logic && Modify Error.js logic

--- src/components/MarketList.js
+++ src/components/MarketList.js
@@ -1,8 +1,67 @@
 import React from "react";
-// import { Loading, Card, Icon, Tag } from "element-react";
+import { graphqlOperation } from "aws-amplify";
+import { Connect } from "aws-amplify-react";
+import { listMarkets } from "../graphql/queries";
+import { Loading, Card, Icon, Tag } from "element-react";
+import { Link } from "react-router-dom";
+import Error from "./Error";
 
 const MarketList = () => {
-  return <div>MarketList</div>;
+  return (
+    <Connect
+      query={graphqlOperation(listMarkets)}
+    >
+      {({ data, loading, errors }) => {
+        if (errors.length > 0) return <Error errors={errors} />;
+        if (loading || !data.listMarkets) return <Loading fullscreen={true} />
+
+        return (
+          // console.log(data)
+          <React.Fragment>
+            <h2 className="header">
+              <img src="https://icon.now.sh/store_mall_directory/527FFF" alt="Store Icon" className="large-icon"/>
+              Markets
+            </h2>
+            {data.listMarkets.items.map(market => (
+              <div key={market.id} className={"my-2"}>
+                <Card
+                  bodyStyle={{
+                    padding: "0.7em",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between"
+                  }}
+                >
+                  <div>
+                    <span className={"flex"}>
+                      <Link className={"link"} to={`/market/${market.id}`}>
+                        {market.name}
+                      </Link>
+                      <span style={{ color: "var(--darkAmazonOrange" }}>
+                        {/*{market.products.items.length}*/}
+                        {market.products.length}
+                      </span>
+                      <img src={"https://icon.now.sh/shopping_cart/f60"} alt={"Shopping Cart"} />
+                    </span>
+                    <div style={{ color: "var(--lightSquidInk" }}>
+                      {market.owner}
+                    </div>
+                  </div>
+                  <div>
+                    {market.tags && market.tags.map(tag => (
+                      <Tag key={tag} type={"danger"} className={"mx-1"}>
+                        {tag}
+                      </Tag>
+                    ))}
+                  </div>
+                </Card>
+              </div>
+            ))}
+          </React.Fragment>
+        );
+      }}
+    </Connect>
+  );
 };
 
 export default MarketList;
